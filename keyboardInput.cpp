16081ea04e5e1f611cb5b36f386b04bc1ddaac6d
Vincent Fraccaro <vincentfraccaro@gmail.com>
2021-08-15 23:52:08 +1000

    Idk just updating

---
+++ keyboardInput.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vincent on 15/08/2021.
+//
+
+#include "keyboardInput.hpp"
---
+++ keyboardInput.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Vincent on 15/08/2021.
+//
+
+#ifndef VOXELS_KEYBOARDINPUT_HPP
+#define VOXELS_KEYBOARDINPUT_HPP
+
+
+class keyboardInput {
+
+};
+
+
+#endif //VOXELS_KEYBOARDINPUT_HPP
